cfc750365b478116e588cdb1f80c4bd97f402c08
Ivan Shishkin <i.shishkin@atomgroup.io>
2025-03-10 15:23:58 +0300

    Fix player mover

--- Assets/_App/Scripts/Multiplayer/Player/NetworkPlayer.cs
+++ Assets/_App/Scripts/Multiplayer/Player/NetworkPlayer.cs
@@ -9,18 +9,21 @@ namespace Tirlim.Multiplayer
     {
         [SerializeField] private IKTargetsTracking ikTargetsTracking;
         [SerializeField] private CalibrateController calibrateController;
+        [SerializeField] private PlayerMover playerMover;
         
         private void Awake()
         {
             ProjectContext.Instance.Container.InjectGameObject(gameObject);//TODO УБРАТЬ
             ikTargetsTracking.enabled = false;
             calibrateController.enabled = false;
+            playerMover.enabled = false;
         }
 
         public override void OnStartAuthority()
         {
             ikTargetsTracking.enabled = true;
             calibrateController.enabled = true;
+            playerMover.enabled = true;
         }
     }
 }
